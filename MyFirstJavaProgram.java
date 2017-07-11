b772cbeaa206718a5c0073772c4497924bd9c60b
Isaacpants <dave@boyermoving.com>
2017-07-11 11:59:23 -0700

    Add files via upload

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,18 @@
+package day1.robot;
+
+import java.awt.Color;
+import org.jointheleague.graphical.robot.Robot;
+
+public class MyFirstJavaProgram {
+	public static void main(String[] args) {
+		
+		// START HERE
+Robot r2d2=new Robot();
+r2d2.penDown();
+for (int i = 0; i < 4; i++) {
+r2d2.turn(90);
+r2d2.move(60);	
+r2d2.
+}
+	}
+}
