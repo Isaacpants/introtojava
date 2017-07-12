af20b8b38a91cb3ffbd27a8f8509fc7a78464c97
Isaacpants <dave@boyermoving.com>
2017-07-12 11:59:07 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {public static void main(String[] args) {
+System.out.println("ledleledleledlelee");
+JOptionPane.showMessageDialog(null, "Hello,World");
+String name=JOptionPane.showInputDialog("Enter your name");
+JOptionPane.showMessageDialog(null, "hello "+name+"!");
+
+
+
+
+}
+}
